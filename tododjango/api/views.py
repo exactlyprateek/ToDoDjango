588c45df3eb13f2b6b05da9fb64fb38275e0593e
exactlyprateek <prateekrohilla4.pr@gmail.com>
2020-06-06 17:55:23 +0530

    views updated

--- tododjango/api/views.py
+++ tododjango/api/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
 # Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import TaskSerializer
+
+from .models import Task
+
+
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+        'List': '/task-list/',
+        'Detail View': '/task-list/<str:pk>/',
+        'Create': '/task-create/',
+        'Update': '/task-update/<str:pk>/',
+        'Delete': '/task-delete/<str:pk>/',
+    }
+    return Response(api_urls)
+
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
