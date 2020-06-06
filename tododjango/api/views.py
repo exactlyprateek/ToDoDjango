da9e5bae29947669b21d1ab7c966baadabe14dcc
exactlyprateek <prateekrohilla4.pr@gmail.com>
2020-06-06 18:32:47 +0530

    crud functionality completed

--- tododjango/api/views.py
+++ tododjango/api/views.py
@@ -12,7 +12,7 @@ from .models import Task
 def apiOverview(request):
     api_urls = {
         'List': '/task-list/',
-        'Detail View': '/task-list/<str:pk>/',
+        'Detail View': '/task-detail/<str:pk>/',
         'Create': '/task-create/',
         'Update': '/task-update/<str:pk>/',
         'Delete': '/task-delete/<str:pk>/',
@@ -25,3 +25,39 @@ def taskList(request):
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+def taskDetail(request, pk):
+    tasks = Task.objects.get(id=pk)
+    serializer = TaskSerializer(tasks, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def taskCreate(request):
+    serializer = TaskSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def taskUpdate(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(instance=task, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def taskDelete(request, pk):
+    task = Task.objects.get(id=pk)
+    task.delete()
+
+    return Response("Item deleted")
