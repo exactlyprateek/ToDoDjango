505a2721467cead948f978418f76ebd1aa2a7619
Prateek Rohilla <prateekrohilla4.pr@gmail.com>
2020-06-09 13:58:17 +0530

    build added

---
+++ tododjango/frontend/build/precache-manifest.72d0430e0f5f60a5dd4669dfb68cbf36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aacb186f6221f3303be473db1595333",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1964b331e1551f2d41d5",
+    "url": "/static/css/main.fdcb2971.chunk.css"
+  },
+  {
+    "revision": "7064a0ee239f1099f196",
+    "url": "/static/js/2.017b0b4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.017b0b4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1964b331e1551f2d41d5",
+    "url": "/static/js/main.e2abb3f0.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  }
+]);
